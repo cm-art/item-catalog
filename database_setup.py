bb11ce1101a3b1ec1826dc7369bc788f40258184
Chris Martin <cmart@oath.com>
2018-07-31 00:14:00 -0400

    DB creation completed

--- database_setup.py
+++ database_setup.py
@@ -1,29 +1,23 @@
 import os
 import sys
 
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from datetime import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
 
 Base = declarative_base()
+
 
 class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True)
     email = Column(String(320), unique=True, nullable=False)
-    first_name = Column(String(40), nullable=False)
-    last_name = Column(String(40), nullable=False)
+    name = Column(String(80), nullable=False)
     building = Column(String(3), nullable=False)
-
-
-class Assets(Base):
-    __tablename__ = 'asset'
-
-    tag = Column(String(20), primary_key=True)
-    status = Column(String(20), nullable=False)
-    hostname = Column(String(20), nullable=False)
 
 
 class Drive(Base):
@@ -32,11 +26,9 @@ class Drive(Base):
     serialno = Column(String(40), primary_key=True)
     manf = Column(String(40), nullable=False)
     model = Column(String(40), nullable=False)
-    wipe_status = Column(DateTime, nullable=False)
-    wipe_start = Column(DateTime, nullable=False)
-    wipe_end = Column(String(10), nullable=False)
-    asset_tag = Column(String(20), ForeignKey('asset.tag'))
-    asset = relationship(Asset)
+    wipe_status = Column(String(10), nullable=False)
+    wipe_start = Column(DateTime)
+    wipe_end = Column(DateTime)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
@@ -44,3 +36,29 @@ class Drive(Base):
 engine = create_engine('sqlite:///drivewipe.db')
 
 Base.metadata.create_all(engine)
+
+engine = create_engine('sqlite:///drivewipe.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+myFirstUser = User(
+    name="Chris Martin",
+    email="cmart@oath.com",
+    building="bf2")
+session.add(myFirstUser)
+session.commit()
+
+myFirstDrive = Drive(
+    serialno="S2Z5NY0HC12345",
+    model="SM0256G",
+    manf="Apple",
+    wipe_status="Dirty",
+    wipe_start=datetime.now(),
+    wipe_end=datetime.now(),
+    user_id="1")
+session.add(myFirstDrive)
+session.commit()
+
+firstResult = session.query(User).first()
+print("Database Setup completed, first user added:", firstResult.name)
